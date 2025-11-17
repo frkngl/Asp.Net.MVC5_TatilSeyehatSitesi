360de05a494ebf9474b66d381c6d3f33eb0276df
frkngl <frknglww@gmail.com>
2025-11-17 21:11:17 +0300

    Hakkimizda ve Iletisim sayafaları için veri tabanından verileri çekilmiştir ve hazır hale gelmiştir.

--- Asp.Net.MVC5_TatilSeyehatSitesi/Controllers/HomeController.cs
+++ Asp.Net.MVC5_TatilSeyehatSitesi/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Asp.Net.MVC5_TatilSeyehatSitesi.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,19 +11,25 @@ namespace Asp.Net.MVC5_TatilSeyehatSitesi.Controllers
 {
     public class HomeController : Controller
     {
+        TatilSeyehatMVC5Entities db = new TatilSeyehatMVC5Entities();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        //Veritabanı işlemleri G/Ç Yoğun olduğu için, uygulamanızın gelecekteki performansını ve ölçeklenebilirliğini sağlamak adına async Task<ActionResult> yapısı kullanılır.
+        public async Task<ActionResult> About()
         {
-            return View();
+            var degerler = await db.TBLABOUT.FirstOrDefaultAsync();
+
+            return View(degerler);
         }
+
         public ActionResult Contact()
         {
-            return View();
+            var degerler = db.TBLCONTACT.FirstOrDefault();
+            return View(degerler);
         }
     }
 }
