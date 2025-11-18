dd9c22acd6ab454ee3720cedf5c23c2adc68cb39
frkngl <frknglww@gmail.com>
2025-11-18 12:30:38 +0300

    Bloglar sayfası düzenlendi ve veriler veri tabanindan çekildi.

---
+++ Asp.Net.MVC5_TatilSeyehatSitesi/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.Net.MVC5_TatilSeyehatSitesi.Models;
+using PagedList; 
+using PagedList.Mvc;
+
+namespace Asp.Net.MVC5_TatilSeyehatSitesi.Controllers
+{
+    public class BlogController : Controller
+    {
+        TatilSeyehatMVC5Entities db = new TatilSeyehatMVC5Entities();
+        TableList data = new TableList();
+        // GET: Blog
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            data.Blogs = db.TBLBLOG.OrderByDescending(x => x.DATE).ToPagedList(pageNumber, pageSize);
+            data.BlogComment = db.TBLBLOGCOMMENTS.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Asp.Net.MVC5_TatilSeyehatSitesi/Models/TableList.cs
@@ -0,0 +1,14 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net.MVC5_TatilSeyehatSitesi.Models
+{
+    public class TableList
+    {
+        public IPagedList<TBLBLOG> Blogs { get; set; }
+        public List<TBLBLOGCOMMENTS> BlogComment { get; set; }
+    }
+}
